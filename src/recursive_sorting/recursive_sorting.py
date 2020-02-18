7869133508b7f0a4fe447842ba8eea3e559f2cfd
Folasade Agbaje <folasadeagbaje@gmail.com>
2020-02-18 13:28:27 +0100

    completed merge sort

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -8,18 +8,39 @@ def merge( arrA, arrB ):
     merged_arr = []
    
     #check which array should come first
-    if arrA[0] > arrB[0]:
-      merged_arr.append(arrA)
-    else:
-      merged_arr.append(arrB)
+    while len(arrA) and len(arrB) != 0:  
+      if arrA[0] > arrB[0]:
+        merged_arr.append(arrA)
+      else:
+        merged_arr.append(arrB)
 
-    
+
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
+
+"""
+Divide the original array in half till only subarrays of 1 are left, index 0, base case
+if arr[0] < arr[0], stay same and merge, else swap and merge, recursive call
+repeat the same process till all have been merged
+
+
+"""
+
 def merge_sort( arr ):
     # TO-DO
+    #base case, when only one item should be in the array
+    if len(arr) < 2:
+      return arr
+    else:
+      #divide the array into halves
+      arr_half = len(arr)//2
+      left_arr = arr[:arr_half]
+      right_arr = arr[arr_half:len(arr)]
+
+      #using the merge function above to combine the 2 arrays
+      return merge(left_arr, right_arr)
 
     return arr
 
